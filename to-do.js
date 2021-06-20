e0c614a50632aee06c82f059b224b86eedf1bf1c
mckent05 <atemitope95@gmail.com>
2021-06-20 22:38:03 +0100

    i started to comment on my code

--- to-do.js
+++ to-do.js
@@ -42,153 +42,156 @@ let clrBtn=document.querySelector('.clr-btn')
 let editElement
 let editFlag=false
 let editID=''
-
+// to add my event listeners
 form.addEventListener('submit', addItems)
 clrBtn.addEventListener('click', clearItems)
-window.addEventListener('DOMContentLoaded', loadItems)
+window.addEventListener('DOMContentLoaded', loadLocalstorage)
 
+
+// add-items function
 function addItems(e){
     e.preventDefault()
     value=toDo.value
-    let id=new Date().getTime().toString()
-    if(value && !editFlag){
-        displayItems(id, value )
-        
-        displayAlert('Event added!','success')
-        toDefault()
+    let id= new Date().getTime().toString()
+    // value and editflag is false
+    if (value && !editFlag){
+        //create a new article element
+        setUpItems(id, value)
+        displayAlert('todo event succesfully added!','success')
+        setDefault()
         addToLocalStorage(id, value)
         
-        
-
-
     }
-    else if(value && editFlag){
+    // value and editing
+    else if (value && editFlag){
         editElement.innerHTML=value
         id=editID
-        displayAlert('edited to-do event!','success')
-        toDefault()
+        displayAlert('todo event edited','success')
+        setDefault()
         editLocalStorage(id, editElement.innerHTML)
     }
+    // when there is no value entered
     else{
-        displayAlert('please enter an event!', 'danger')
-        toDefault()
+        displayAlert('to do event cannot be empty!', 'danger')
+        setDefault()
     }
 }
+
+// alert function for actions taking place
 function displayAlert(text, action){
     alert.innerHTML=text
     alert.classList.add(`alert-${action}`)
 
+
+// display alert timeout
     setTimeout(function(){
         alert.innerHTML=''
         alert.classList.remove(`alert-${action}`)
-   }, 1000)
+    }, 1000)
 }
-function toDefault(){
-    toDo.value=""
+// set parameters back to default
+function setDefault(){
+    toDo.value=''
     editFlag=false
     editID=''
     submit.innerHTML='submit'
 }
+//clear items in to-do list
 function clearItems(){
-    let events=document.querySelectorAll('.list')
-    events.forEach(function(event){
-        list.removeChild(event)
+    let items=document.querySelectorAll('.list')
+    items.forEach(function(item){
+        list.removeChild(item)
     })
     container.classList.remove('show-list')
-    displayAlert('to-do list empty!','danger')
-    toDefault()
+    displayAlert('todo list cleared!','danger')
     clearLocalStorage()
-    
 }
+// delete item from todo list
 function deleteItems(e){
-    let target=e.currentTarget.parentElement.parentElement
+    let target= e.currentTarget.parentElement.parentElement
     let id=target.dataset.id
     list.removeChild(target)
-    if(list.children.length===0){
+    if (list.children.length===0){
         container.classList.remove('show-list')
+
     }
-    displayAlert('your to-do deleted!','danger')
-    toDefault()
-    deleteFromLocalStorage(id)
+    displayAlert('todo event deleted!','danger')
+    setDefault()
+    deleteFromlocalstorage(id)
 }
+// edit item in list
 function editItems(e){
     editFlag=true
     let target=e.currentTarget.parentElement.parentElement
-    editElement=e.currentTarget.parentElement.previousElementSibling
     editID=target.dataset.id
+    editElement=e.currentTarget.parentElement.previousElementSibling
     toDo.value=editElement.innerHTML
     submit.innerHTML='edit'
-   
     
 }
-function addToLocalStorage(id, value){
-    let toDo={id, value}
-    let items=getLocalStorage()
+// add to Local Storage
+function addToLocalStorage(id,value){
+    let toDo= {id, value}
+    let items= getLocalStorage()
     items.push(toDo)
-    
-    localStorage.setItem('mylist', JSON.stringify(items))
-    
-}
-function getLocalStorage(){
-    
-    if(localStorage.getItem('mylist') ==null){
-       return []
-    }
-    else{
-        return JSON.parse(localStorage.getItem('mylist'))
-    }
-    // return localStorage.getItem('mylist') ? JSON.parse(localStorage.getItem('mylist')) : []
 
+    localStorage.setItem('mylist', JSON.stringify(items))
 }
-function deleteFromLocalStorage(id){
-    let items = getLocalStorage()
+// get items from local storage
+function getLocalStorage(){
+    return localStorage.getItem('mylist') ? JSON.parse(localStorage.getItem('mylist')):[]
+}
+// clear local storage
+function clearLocalStorage(){
+    let items=getLocalStorage()
+    localStorage.clear(items)
+}
+// delete from local storage
+function deleteFromlocalstorage(id){
+    let items=getLocalStorage()
     items=items.filter(function(item){
-        if(item.id != id){
+        if(item.id !== id){
             return item
         }
-        
     })
     localStorage.setItem('mylist', JSON.stringify(items))
 }
-function clearLocalStorage(){
-    localStorage.clear('mylist')
-}
+//edit Localstorage
 function editLocalStorage(id, value){
     let items=getLocalStorage()
     items=items.map(function(item){
-        if(item.id == id){
-            item.value= value
+        if(item.id==id){
+            item.value=value
         }
         return item
     })
     localStorage.setItem('mylist', JSON.stringify(items))
 }
-function loadItems(){
+//load DOM content from local storage
+function loadLocalstorage(){
     let items=getLocalStorage()
     items.forEach(function(item){
-        displayItems(item.id, item.value)
+        setUpItems(item.id, item.value)
     })
-   
-    
 }
-
-function displayItems(id, value){
-        let article=document.createElement('article')
+// set up items
+function setUpItems(id, value){
+    let article=document.createElement('article')
         article.classList.add('list')
-        let dataID=document.createAttribute('data-id')
-        dataID.value=id
-        article.setAttributeNode(dataID)
-        article.innerHTML=` <p>${value}</p>
+        let artId=document.createAttribute('data-id')
+        artId.value=id
+        article.setAttributeNode(artId)
+        article.innerHTML= ` <p>${value}</p>
         <div class="btn-container">
-            <button class="edit-btn">edit</button>
-            <button class="delete-btn">delete</button>
-        </div>`
-        let deleteBtn=article.querySelector('.delete-btn')
+        <button class="edit-btn">edit</button>
+    <button class="delete-btn">delete</button>
+    </div>`
+        //set event listeners for my edit and delete button
+        let delBtn=article.querySelector('.delete-btn')
         let editBtn=article.querySelector('.edit-btn')
-        deleteBtn.addEventListener('click', deleteItems)
-        editBtn.addEventListener('click', editItems) 
+
+        delBtn.addEventListener('click', deleteItems)
+        editBtn.addEventListener('click', editItems)
         list.appendChild(article)
         container.classList.add('show-list')
-
-
 }
